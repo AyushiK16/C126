0761d07fa89b7bc80388092b8a094861969477e7
System Administrator <root@Ayushis-Rose-Air.local>
2022-01-23 11:15:30 +0530

    Initial

---
+++ App.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import PickImage from './screens/camera.js'
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <PickImage>
+        
+      </PickImage>
+    )
+  }
+}
---
+++ screens/camera.js
@@ -0,0 +1,86 @@
+import * as React from 'react'
+import {Button, View, Platform} from 'react-native'
+import * as ImagePicker from 'expo-image-picker'
+import * as Permissions from 'expo-permissions'
+
+export default class PickImage extends React.Component{
+    render(){
+        return(
+            <View style = {{felx:1, alignItems:'center', justifyContent:'center'}}>
+                <Button
+                title = 'Pick an Image From Gallary'
+                onPress = {this.pickImage}>
+
+                </Button>
+            </View>
+        )
+    }
+
+    getPermission = async() => {
+        if(Platform.OS != 'web'){
+            const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL)
+            if(status != 'granted'){
+                alert('Sorry, we need camera permission.')
+            }
+        }
+    }
+
+    componentDidMount(){
+        this.getPermission()
+    }
+
+    pickImage = async() => {
+        try{
+            var result = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.All,
+                allowsEditing: true,
+                aspect: [4,3],
+                quality: 1
+            })
+
+            if(!result.cancelled){
+                console.log('result uri == ', result.uri)
+                this.uploadImage(result.uri)
+            }
+
+        }
+
+        catch(error){
+            console.log(error)
+        }
+    }
+
+    uploadImage = async(uri) => {
+        const data = new FormData()
+        var fileName = uri.split('/')[uri.split('/').length - 1]
+        var type = `image/${uri.split('.')[uri.split('.').length - 1]}`
+        console.log(type, 'type of upload image')
+
+        const file = {
+            uri : uri,
+            name : fileName,
+            type : type
+        }
+
+        data.append('digit', file)
+        fetch('https://818b-2405-201-4012-703f-6198-d2bb-9a90-1726.ngrok.io',{
+            method: 'POST',
+            body : data,
+            headers : {
+                'content-type' : 'multipart/form-data'
+            }
+        })
+        .then((response)=>{
+            return response.json()
+            //to know if the api is successfully called
+        })
+        .then((result)=>{
+            console.log('result success!', result)
+            //the digit predicted
+        })
+        .catch((error)=>{
+            console.log('error in upload image api', error)
+            //any error
+        })
+    }
+}
